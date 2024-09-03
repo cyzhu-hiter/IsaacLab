e9328f040852241ae8508b139311e6380806449d
Chay <hitzcy@126.com>
2024-09-03 01:43:21 -0400

    bug fix

--- source/training_schedule/common/run_training.py
+++ source/training_schedule/common/run_training.py
@@ -52,7 +52,7 @@ def run_training(task, num_envs, nproc_per_node, mini_epoch, minibatch_size, see
         f"source/standalone/workflows/rl_games/train.py --task={task} "
         f"--headless --distributed --wandb --pre-ex --num_envs {num_envs} "
         f"--mini_epoch {mini_epoch} --minibatch_size {minibatch_size} "
-        f"--seed {seed} --tuning_param {tuning_param} --max_time {max_time} --max_iterations {max_iterations}"
+        f"--seed {seed} --tuning_param {tuning_param} --max_time {max_time} --max_iterations {int(max_iterations)}"
     )
     
     print(f"Running task: {task}, Tuning {tuning_param}={tuning_value}, Seed={seed}")
